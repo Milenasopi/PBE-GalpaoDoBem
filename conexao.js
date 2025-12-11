9c2fe2f040cd62fc912b38eb70f42a738dfaf214
Milena <milenasopi@gmail.com>
2025-12-11 12:35:27 -0300

    Ajustando para a Vercel

--- conexao.js
+++ conexao.js
@@ -3,27 +3,27 @@ require('dotenv').config();
 // indica q vamos usar o banco de dados pg
 const { Pool } = require('pg');
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
-});
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT
+// });
 
 // pra poder usar a conexão em outros lugares
 // module.exports = pool;
 
 
 //para conexão com a Vercel
-// require('dotenv').config();
-// const { Pool } = require('pg');
+require('dotenv').config();
+const { Pool } = require('pg');
 
-// const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
-// });
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 module.exports = pool;
